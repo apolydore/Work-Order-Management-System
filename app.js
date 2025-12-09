55f3217cd2a8caa7b9093e6418e132bc681c5ff2
apolydore <meliap.360@outlook.com>
2025-12-09 02:00:03 -0400

    Added the dashbard UI with notification and statistics pages. Created pages.js to render handlebar views and registered the pages route in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 import configRoutes from './routes/index.js';
 import notificationsRouter from './routes/notifications.js';
 import statisticsRouter from './routes/statistics.js';
+import pagesRouter from './routes/pages.js';
 
 app.use(express.json());
 
@@ -27,6 +28,9 @@ app.use('/api/notifications', notificationsRouter);
 
 // for statistics summary
 app.use('/api/statistics', statisticsRouter);
+
+// for pages.js
+app.use('/', pagesRouter);
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
