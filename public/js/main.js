af91e960b1dc9fe4161862d6c425c7c13a0c7244
cquanshau <catherinequanshau@outlook.com>
2025-12-10 10:35:14 -0500

    styled signup page and login form

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('login');
+    const getStartedBtn = document.getElementById('getStarted');
+    const jobRequestBtn = document.getElementById('jobRequest');
 
     if (loginBtn) {
         loginBtn.addEventListener('click', () => {
             window.location.href='/login';
         });
+    }
+
+    if (getStartedBtn) {
+        getStartedBtn.addEventListener('click', () => {
+            window.location.href='/signup';
+        });
+    }
+
+    if (jobRequestBtn) {
+        jobRequestBtn.addEventListener('click', () => {
+            window.location.href='#'; 
+        }); 
     }
 }); 
